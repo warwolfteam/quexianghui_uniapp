e469ceea91dba9b5504860688eb69bf15ad645a3
Mr_heimaojun <zxx@heimaoba.cn>
2022-12-13 09:49:33 +0800

    导入基础配置

---
+++ config/app.js
@@ -0,0 +1,29 @@
+
+let domain = 'http://192.168.110.132:8081'
+// let domain = 'http://api.java.crmeb.net:20001'
+module.exports = {
+	// 请求域名 格式： https://您的域名
+	// #ifdef MP
+		HTTP_REQUEST_URL: domain,
+	// #endif
+	HTTP_ADMIN_URL:'http://http://192.168.110.132:8083', //PC后台的API请求地址，上传图片用
+	// #ifdef H5
+		//H5接口是浏览器地址
+		// HTTP_REQUEST_URL: window.location.protocol+"//"+window.location.host,
+		// http://api.java.crmeb.net:20001
+		HTTP_REQUEST_URL:domain,
+	// #endif
+	
+	HEADER:{
+		'content-type': 'application/json'
+	},
+	HEADERPARAMS:{
+		'content-type': 'application/x-www-form-urlencoded'
+	},
+	// 回话密钥名称 请勿修改此配置
+	TOKENNAME: 'Authori-zation',
+	// 缓存时间 0 永久
+	EXPIRE:0,
+	//分页最多显示条数
+	LIMIT: 10
+};
---
+++ config/cache.js
@@ -0,0 +1,34 @@
+module.exports = {
+        //token
+        LOGIN_STATUS: 'LOGIN_STATUS_TOKEN',
+        // uid
+        UID:'UID',
+        //用户信息
+        USER_INFO: 'USER_INFO',
+        //token 过期时间
+        EXPIRES_TIME: 'EXPIRES_TIME',
+        //微信授权
+        WX_AUTH: 'WX_AUTH',
+        //微信授权状态
+        STATE_KEY: 'wx_authorize_state',
+        //登录类型
+        LOGINTYPE: 'loginType',
+        //登录回调地址
+        BACK_URL: 'login_back_url',
+        // 小程序授权状态
+        STATE_R_KEY: 'roution_authorize_state',
+        //logo Url
+        LOGO_URL: 'LOGO_URL',
+        //模板缓存
+        // SUBSCRIBE_MESSAGE: 'SUBSCRIBE_MESSAGE',
+
+        TIPS_KEY: 'TIPS_KEY',
+
+        SPREAD: 'spread',
+        //缓存经度
+        CACHE_LONGITUDE: 'LONGITUDE',
+        //缓存纬度
+        CACHE_LATITUDE: 'LATITUDE',
+		 //app手机信息
+		PLATFORM: 'systemPlatform'
+}
